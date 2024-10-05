d879eec3d9629f146ff05edff616a4a110ca677c
idncod <idncod@protonmail.com>
2024-10-05 20:36:56 +0100

    add mixpanel event tracker 'Page Visit'

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
+'use client'
+import { useEffect } from 'react';
 import localFont from "next/font/local";
 import "./globals.css";
-import MobileMenu from "@/components/Menu/MobileMenu/MobileMenu";
+import Header from "@/components/Header/Header";
+import mixpanel from 'mixpanel-browser';
+import { metadata } from './metadata';
+import Router from "next/router";
+import { usePathname } from "next/navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,17 +19,48 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata = {
-  title: "Aquamarine Bloom Hobby Cattery",
-  description: "Hobby breeders of Elf cats with odd eyes in London",
-};
-
 export default function RootLayout({ children }) {
+    const pathname = usePathname();
+
+    useEffect(() => {
+        mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN);
+
+        const trackPageView = () => {
+            if(mixpanel) {
+                mixpanel.track("Page Visit", {
+                    page: pathname,
+                    title: document.title,
+                });
+            } else {
+                console.error("Mixpanel not init")
+            }
+        };
+
+        trackPageView();
+
+        const handleRouteChange = (url) => {
+            mixpanel.track("Page Visit", {
+                page: url,
+                title: document.title,
+            });
+        };
+
+        Router.events.on("routeChangeComplete", handleRouteChange);
+
+        return () => {
+            Router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [pathname])
   return (
+
     <html lang="en">
+      <head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description}/>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      <MobileMenu />
-        {children}
+      <Header />
+      {children}
       </body>
     </html>
   );
---
+++ src/app/metadata.js
@@ -0,0 +1,4 @@
+export const metadata = {
+    title: "Aquamarine Bloom Hobby Cattery",
+    description: "Hobby breeders of Elf cats with odd eyes in London",
+};
